07c5a209f61276f5a5244ebf6148e97ae1572c6c
Jakub Kowalski <kubabartek1@op.pl>
2017-04-28 11:16:03 +0200

    First ZipCode commit

---
+++ src/Zad2.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Jakub Kowalski on 28.04.2017.
+ */
+public class Zad2{
+    public static void main(String[] args) {
+        int number = 1;
+        int numberOfDiv=1;
+        int i;
+        while (numberOfDiv!=20)
+        {
+            if(number%numberOfDiv==0)
+            {
+                numberOfDiv++;
+            }else
+            {
+                number++;
+                numberOfDiv=1;
+            }
+        }
+        System.out.println(number);
+    }
+
+}
+
